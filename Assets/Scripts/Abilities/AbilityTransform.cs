12d690971f4fe40bbfc71c13dc76c3faa5aa1456
DHindriks <dl.hindriks@gmail.com>
2021-10-10 14:37:16 +0200

    small fixes. added fortress spawning system. added ability system

---
+++ Assets/Scripts/Abilities/AbilityTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityTransform : AbilityBase
+{
+    [SerializeField]
+    GameObject TransformInto;
+
+    void OnMouseDown()
+    {
+        if (!AbilityLocked)
+        {
+            GameObject Transformed = Instantiate(TransformInto);
+            Transformed.transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/SlingShotScript.cs
+++ Assets/Scripts/SlingShotScript.cs
@@ -16,6 +16,15 @@ public class SlingShotScript : MonoBehaviour
 
     bool IsShot = false;
 
+    float Currentcooldown = 1;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }    
+    }
 
     // Update is called once per frame
     void Update()
@@ -38,19 +47,43 @@ public class SlingShotScript : MonoBehaviour
                     ballRB.position = mousePos;
                 }
             }
-            else if (ballRB.position.y < -10)
-            {
-                Destroy(ballRB.gameObject);
-            }
 
             //shoots the object
             if (Input.GetMouseButtonUp(0) && Vector3.Distance(ballRB.position, Origin.transform.position) > 0.3f)
             {
                 IsShot = true;
                 ballRB.isKinematic = false;
-                ballRB.AddRelativeTorque(new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10)), ForceMode.VelocityChange);
+                ballRB.AddRelativeTorque(new Vector3(Random.Range(0, 0), Random.Range(0, 0), Random.Range(0, 10)), ForceMode.VelocityChange);
                 ballRB.AddForce(((transform.position - ballRB.transform.position) * 1000) * ballRB.mass);
+                if (ballRB.gameObject.GetComponent<AbilityBase>())
+                {
+                    ballRB.GetComponent<AbilityBase>().AbilityLocked = false;
+                }
+                ballRB = null;
+                Currentcooldown = Time.time + 1.5f;
             }
         }
     }
+
+    void SetProjectile (Rigidbody rb)
+    {
+        rb.isKinematic = true;
+        rb.gameObject.transform.position = transform.position;
+        ballRB = rb;
+        IsShot = false;
+        if (ballRB.gameObject.GetComponent<AbilityBase>())
+        {
+            ballRB.GetComponent<AbilityBase>().AbilityLocked = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Rigidbody>() && ballRB == null && Currentcooldown < Time.time)
+        {
+            SetProjectile(other.gameObject.GetComponent<Rigidbody>());
+        }    
+    }
+
+
 }
